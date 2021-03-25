3048250759bb235946e561888f9ba45d69481d70
Lukas <lukas.osaki@gmail.com>
2021-03-25 12:27:36 -0300

    correção bug client seed

--- database/seeds/0002_ClientSeeder.js
+++ database/seeds/0002_ClientSeeder.js
@@ -10,15 +10,18 @@
 |
 */
 
-/** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
 const Role = use('Role')
 const User = use('App/Models/User')
 class ClientSeeder {
   async run() {
     const role = await Role.findBy('slug', 'client')
-    const Clients = await Factory.model('App/Models/User').createMany(30)
-    await Promise.all(client.map(async client => {
+    const clients = await Factory
+      .model('App/Models/User')
+      .createMany(30)
+
+    await Promise.all(
+      clients.map(async client => {
       await client.roles().attach([role.id])
     }))
 
@@ -29,7 +32,7 @@ class ClientSeeder {
       password: 'secret'
     })
 
-    const adminRole = await Role.findBy('slug', admin)
+    const adminRole = await Role.findBy('slug', 'admin')
     await user.roles().attach([role.id])
 
   }
