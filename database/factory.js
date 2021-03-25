81bd75bc2224a214787b2bb9fc9e23c817ab4032
Lukas <lukas.osaki@gmail.com>
2021-03-25 10:05:45 -0300

    Fakers

--- database/factory.js
+++ database/factory.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Factory = require('@adonisjs/lucid/src/Factory')
+
 /*
 |--------------------------------------------------------------------------
 | Factory
@@ -14,8 +16,26 @@
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 // const Factory = use('Factory')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/User', faker => {
+    return {
+        name: faker.first(),
+        surname: faker.last(),
+        email: faker.email({ domain: 'osaki.com'}),
+        password: 'secret'
+    }
+}) 
+
+Factory.blueprint('App/Models/Category', faker => {
+    return {
+        title: faker.country({ full: true}),
+        description: faker.sentence()
+    }
+})
+
+Factory.blueprint('App/Models/Product', faker => {
+    return {
+        name: faker.animal(),
+        description: faker.sentence(),
+        price: faker.float({ min: 0, max: 1000, fixed: 2})
+    }
+})
